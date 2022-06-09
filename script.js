f2fbccafc552ca092c7615520c1b53264b6237ab
unknown <thomas_ketler@web.de>
2022-06-10 00:29:58 +0200

    add html css java script

---
+++ script.js
@@ -0,0 +1,58 @@
+let questions = [
+    {
+        "question": "Was hält die Freiheitsstatue in ihrer linken Hand?",
+        "answer_1": "Eine Fackel",
+        "answer_2": "Ein Kürbis",
+        "answer_3": "Eine Tabula ansata mit dem Datum der amerikanischen Unabhängigkeitserklärung",
+        "answer_4": "Ein Brett",
+        "right_answer": 3
+    },
+    {
+        "question": "Von wann bis wann dauerte der erste Weltkrieg?",
+        "answer_1": "1655 – 1699",
+        "answer_2": "1914 – 1918",
+        "answer_3": "1875 – 1878",
+        "answer_4": "1722 – 1733",
+        "right_answer": 2
+    },
+    {
+        "question": "An welchem Datum fiel die Berliner Mauer?",
+        "answer_1": "8. November 1980",
+        "answer_2": "5. November 1969",
+        "answer_3": "15. November 1975",
+        "answer_4": "9. November 1989",
+        "right_answer": 4
+    },
+    {
+        "question": "Wie viele Planeten gehören zu unserem Sonnensystem?",
+        "answer_1": "8",
+        "answer_2": "5",
+        "answer_3": "9",
+        "answer_4": "12",
+        "right_answer": 1
+    },
+    {
+        "question": "Wie heißt der freche Kobold von Meister Eder?",
+        "answer_1": "Winnie Puuh",
+        "answer_2": "Pumuckl",
+        "answer_3": "Mickie Maus",
+        "answer_4": "Donald Duck",
+        "right_answer": 2
+    },
+    {
+        "question": "Süßes oder Saures“, wann benutzt man diesen Spruch?",
+        "answer_1": "Weihnachten",
+        "answer_2": "Ostern",
+        "answer_3": "Halloween",
+        "answer_4": "Pfingsten",
+        "right_answer": 3
+    },
+    {
+        "question": "Wie viele Minuten hat eine Stunde?",
+        "answer_1": "45",
+        "answer_2": "70",
+        "answer_3": "57",
+        "answer_4": "60",
+        "right_answer": 4
+    }
+];
