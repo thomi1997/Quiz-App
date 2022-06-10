1f188df6324cef702072681673539c6b4d6073d5
unknown <thomas_ketler@web.de>
2022-06-10 12:46:58 +0200

    added quizapp

--- script.js
+++ script.js
@@ -56,3 +56,69 @@ let questions = [
         "right_answer": 4
     }
 ];
+
+let rightQuestions = 0;
+let currentQuestion = 0;
+
+
+function init() {
+    document.getElementById('all-questions').innerHTML = questions.length;
+    showQuestion();
+}
+
+function showQuestion() {
+    if(currentQuestion >= questions.length) {
+        //TODO: Show End Screen
+        document.getElementById('end-screen').style = '';
+        document.getElementById('question-body').style = 'display: none';
+        document.getElementById('amount-of-questions').innerHTML = questions.length;
+        document.getElementById('amount-of-right-questions').innerHTML = rightQuestions;
+        document.getElementById('header-image').src = 'img/win.png';
+    }else {
+        let question = questions[currentQuestion];
+        document.getElementById('question-number').innerHTML = currentQuestion + 1;
+        document.getElementById('questiontext').innerHTML = question['question'];
+        document.getElementById('answer_1').innerHTML = question['answer_1'];
+        document.getElementById('answer_2').innerHTML = question['answer_2'];
+        document.getElementById('answer_3').innerHTML = question['answer_3'];
+        document.getElementById('answer_4').innerHTML = question['answer_4'];
+    }
+}
+
+
+function answer(selection) {
+    let question = questions[currentQuestion];
+    let selectedQuestionNumber = selection.slice(-1);
+    let idOfRightAnswer = `answer_${question['right_answer']}`;
+
+    if(selectedQuestionNumber == question['right_answer']) { // Richitge Frage beantwortet !
+        document.getElementById(selection).parentNode.classList.add('bg-success');
+        console.log('Richtige Antwort!!');
+        rightQuestions++;
+    }else{
+        document.getElementById(selection).parentNode.classList.add('bg-danger');
+        document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
+        console.log('Falsche Antwort!!');
+    }
+    document.getElementById('next-button').disabled = false;
+}
+
+
+function nextQuestion() {
+    currentQuestion++; // z.B von 0 auf 1
+    document.getElementById('next-button').disabled = true;
+    resetAnswerButtons();
+    showQuestion();
+}
+
+
+function resetAnswerButtons() {
+    document.getElementById('answer_1').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_1').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-success');
+}
